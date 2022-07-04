5cdf53a3c8e45c3d980bf7467c0dea21f3c582ec
Carlos Henrique <carlosh.sales@outlook.com>
2022-07-04 13:40:19 -0300

    Textos sobre mim

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
+
 let aboutText = document.getElementById('aboutText');
 let liAbout = document.querySelectorAll('li');
 
+// import { text } from './text';
 
 liAbout.forEach(item => {
     if (item.id === "") {
@@ -14,8 +16,27 @@ liAbout.forEach(item => {
             }
 
             item.classList.add('active-btn');
+
+            switch (item.id) {
+                case 'easy':
+                    aboutText.innerHTML = `
+                    Olá, me chamo Carlos Henrique, e ser um desenvolvedor sempre foi um sonho, e agora estou me inserindo na carreira como Desenvolvedor Fullstack JS
+                    `;
+                    break;
+                case 'normal':
+                    aboutText.innerHTML = `
+                    Atualmente estou seguindo o curso Fullstack developer na Blue Edtech, e tem foco no JavaScript e suas bibliotecas/frameworks.\n Possuo outras experiências que me ajudam sempre na tentativa de aprender algo novo, já desenvolvi alguns jogos simples em GDScript (liguagem da Godot Engine), passei pelo Java, C/C++/C#, Python...\n Tentei muito até chegar onde estou no momento.
+                    `;
+                    break;
+                case 'hard':
+                    aboutText.innerHTML = `
+                        Hoje tenho todo o tempo necessário para me tornar o desenvolvedor que sempre quis ser, com muito empenho e dedicação, estudo diáriamente várias horas por dia, tanto os conteúdos do curso quanto algumas ramificações para me especializar em outros tipos de tecnologia.
+                    `;
+                    break;
+            }
         });
+
+
     }
 })
-
 
